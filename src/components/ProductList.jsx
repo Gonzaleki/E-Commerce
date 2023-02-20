788bf6ed1ff1d756836635f4386a8662743b5baa
Gonzalo Ledesma <gonzalo.ledesma670@gmail.com>
2023-02-20 20:11:14 -0300

    Styling Card on List Products

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -7,7 +7,7 @@ export default function ProductList() {
   const formatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
-});
+  });
 
   console.log(data);
 
@@ -16,12 +16,12 @@ export default function ProductList() {
       <div className="cards">
         {data && data.map(item =>
         (
-          <div key={item.id} className='card'>
+          <div key={item.id} className='card' onClick={()=>{alert('soon')}}>
             <img src={item.img} alt=''></img>
-            <span className='card__inner'>
+            <span className='product-details'>
               <h3 style={{ marginBottom: 0 }}>{item.name}</h3>
               <h3 style={{ margin: 3 }}>{formatter.format(item.price)}</h3>
-              <button className='add-cart'>ADD TO CART <i className="fa-sharp fa-solid fa-cart-shopping"></i></button>
+              <button className='add-cart'><i className="fa-sharp fa-solid fa-cart-shopping"></i></button>
             </span>
           </div>
         ))
