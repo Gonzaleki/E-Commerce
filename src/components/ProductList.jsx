8021a1dbe80c60364e66772dbc66c4a8899a901b
Gonzalo Ledesma <gonzalo.ledesma670@gmail.com>
2023-03-01 03:35:37 -0300

    Fix Sizes Product List and Home

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -13,15 +13,15 @@ export default function ProductList() {
 
   return (
     <main>
-      <div className="cards">
+      <div className="cards-list">
         {data && data.map(item =>
         (
-          <div key={item.id} className='card' onClick={()=>{alert('soon')}}>
+          <div key={item.id} className='card-list' onClick={()=>{alert('soon')}}>
             <img src={item.img} alt=''></img>
             <span className='product-details'>
               <h3 style={{ marginBottom: 0 }}>{item.name}</h3>
               <h3 style={{ margin: 3 }}>{formatter.format(item.price)}</h3>
-              <button className='add-cart'><i className="fa-sharp fa-solid fa-cart-shopping"></i></button>
+              <button className='add'><i className="fa-sharp fa-solid fa-cart-shopping"></i></button>
             </span>
           </div>
         ))
