4428cc6f5bac51024d7da66c006734d87c00a071
Gonzalo Ledesma <gonzalo.ledesma670@gmail.com>
2023-03-19 21:47:33 -0300

    Options in mapped

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,13 +1,20 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import '../styles/shopping-list.css'
+import { NavOptions } from '../data/NavData'
 
 export default function Filters() {
 
     const [minPrice, setMinPrice] = useState()
+    const [selectedValue, setSelectedValue] = useState('');
 
     const handleChangeMinPrice = (e) => {
         setMinPrice(e.target.value)
-      }
+    }
+    const handleChangeValue = (e) => {
+        setSelectedValue(e.target.value)
+    }
+
+    let mock = NavOptions;
 
     return (
         <section className='filter-panel'>
@@ -18,23 +25,59 @@ export default function Filters() {
             </div>
             <div className='type'>
                 <label htmlFor="type">Type</label>
-                <select name="type" id="type" placeholder='Type'>
+                <select name="type" id="type" onChange={handleChangeValue} placeholder='Type'>
                     <option hidden selected> -- select --</option>
-                    <option value="shirt">Shirt</option>
-                    <option value="shirt">Shirt</option>
-                    <option value="shirt">Shirt</option>
-                    <option value="shirt">Shirt</option>
+                    {NavOptions.map((e) => {
+                        return <option key={e.title} value={e.title}>{e.title}</option>
+                    })}
                 </select>
             </div>
             <div className='subtype'>
                 <label htmlFor="type">SubType</label>
-                <select name="type" id="type" placeholder='Type'>
-                    <option hidden selected> -- select --</option>
-                    <option value="shirt">Shirt</option>
-                    <option value="shirt">Shirt</option>
-                    <option value="shirt">Shirt</option>
-                    <option value="shirt">Shirt</option>
-                </select>
+
+                {selectedValue === 'Shirts' ? (
+                    <select name="type" id="type" placeholder='Type'>
+                        <option hidden selected> -- select --</option>
+                        {NavOptions[0].children.map((e) => {
+                            return <option key={e.title} value={e.title}>{e.title}</option>
+                        })}
+                    </select>
+                ) :
+                    selectedValue === 'Sweters' ? (
+                        <select name="type" id="type" placeholder='Type'>
+                            <option hidden selected> -- select --</option>
+                            {NavOptions[1].children.map((e) => {
+                                return <option key={e.title} value={e.title}>{e.title}</option>
+                            })}
+                        </select>
+                    ) :
+                        selectedValue === 'Jeans' ? (
+                            <select name="type" id="type" placeholder='Type'>
+                                <option hidden selected> -- select --</option>
+                                {NavOptions[2].children.map((e) => {
+                                    return <option key={e.title} value={e.title}>{e.title}</option>
+                                })}
+                            </select>
+                        ) :
+                            selectedValue === 'Pants' ? (
+                                <select name="type" id="type" placeholder='Type'>
+                                    <option hidden selected> -- select --</option>
+                                    {NavOptions[3].children.map((e) => {
+                                        return <option key={e.title} value={e.title}>{e.title}</option>
+                                    })}
+                                </select>
+                            ) :
+                                selectedValue === 'Shorts' ? (
+                                    <select name="type" id="type" placeholder='Type'>
+                                        <option hidden selected> -- select --</option>
+                                        {NavOptions[1].children.map((e) => {
+                                            return <option key={e.title} value={e.title}>{e.title}</option>
+                                        })}
+                                    </select>
+                                )
+                                    : (<></>)
+                }
+
             </div>
             <div>
                 <label htmlFor="tendency">Tendency</label>
